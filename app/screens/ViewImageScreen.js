87b44328a6292a72cd3570cb182be5c2dc542766
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-01-26 13:18:11 -0500

    created colors.js in config and implimented object values in viewimagescreen

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Image, StyleSheet, View } from 'react-native';
 
+import colors from '../config/colors';
+
 function ViewImageScreen(props) {
     return (
         <View style={styles.container}>
-            <View style={styles.close}></View>
-            <View style={styles.delete}></View>
+            <View style={styles.closeIcon}></View>
+            <View style={styles.deleteIcon}></View>
             <Image
                 resizeMode='contain'
                 source={require('../assets/chair.jpg')}
@@ -16,16 +18,30 @@ function ViewImageScreen(props) {
 }
 
 const styles = StyleSheet.create({
+    closeIcon: {
+        height: 50,
+        width: 50,
+        backgroundColor: colors.primary,
+        position: 'absolute',
+        top: 40,
+        left: 30,
+    },
     container: {
         backgroundColor: '#000',
         flex: 1,
     },
-    close: {
+    deleteIcon: {
+        height: 50,
+        width: 50,
+        backgroundColor: colors.secondary,
+        position: 'absolute',
+        top: 40,
+        right: 30,
 
     },
     image: {
         height: '100%',
-        width: '100%'
+        width: '100%',
     },
 })
 
