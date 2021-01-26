e9ca78d2f12849831d30a73f186fe617d6316891
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-01-26 13:25:28 -0500

    added additional colors to colors.js pallette

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
         left: 30,
     },
     container: {
-        backgroundColor: '#000',
+        backgroundColor: colors.black,
         flex: 1,
     },
     deleteIcon: {
