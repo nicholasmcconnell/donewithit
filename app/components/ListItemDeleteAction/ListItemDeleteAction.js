4c0973066b3b9ca0a677e1bd847c76867be0bcdc
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-02-01 12:08:51 -0500

    implimented handle delete function for onpree of listItemDeleteAction component.

--- app/components/ListItemDeleteAction/ListItemDeleteAction.js
+++ app/components/ListItemDeleteAction/ListItemDeleteAction.js
@@ -3,17 +3,20 @@ import { View, StyleSheet } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 import colors from '../../config/colors';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
-export default function ListItemDeleteAction() {
+export default function ListItemDeleteAction({ onPress }) {
     return (
-        <View style={styles.container}>
-            <MaterialCommunityIcons
-                name='trash-can'
-                size={35}
-                color='white'
+        <TouchableWithoutFeedback onPress={onPress}>
+            <View style={styles.container}>
+                <MaterialCommunityIcons
+                    name='trash-can'
+                    size={35}
+                    color='white'
 
-            />
-        </View>
+                />
+            </View>
+        </TouchableWithoutFeedback>
     )
 }
 
@@ -21,6 +24,7 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: colors.danger,
         width: 70,
+        height: '100%',
         justifyContent: 'center',
         alignItems: 'center',
     },
