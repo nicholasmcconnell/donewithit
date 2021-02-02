02247f512ca02827fe2c81fb0fce9b236aea0430
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-02-02 17:13:16 -0500

    added modal to app picker to display categories

--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -1,25 +1,50 @@
-import React from 'react'
-import { TextInput, View, StyleSheet, Platform } from 'react-native'
+import React, { useState } from 'react'
+import { TextInput, View, StyleSheet, TouchableWithoutFeedback, Modal, Button, FlatList } from 'react-native'
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import defaultStyles from '../config/styles'
 import AppText from './AppText.js';
+import Screen from './Screen';
+import ListItemSeperator from './ListItemSeperator';
+import PickerItem from './PickerItem';
 
-function AppPicker({ icon, placeholder, ...otherProps }) {
+function AppPicker({ icon, items, placeholder, ...otherProps }) {
+    const [modalVisible, setModalVisible] = useState(false);
+
     return (
-        <View style={styles.container} >
-            {icon && <MaterialCommunityIcons
-                name={icon}
-                size={20}
-                color={defaultStyles.colors.medium}
-                style={styles.icon} />}
-            <AppText style={styles.text} >{placeholder}</AppText>
-            {icon && <MaterialCommunityIcons
-                name='chevron-down'
-                size={20}
-                color={defaultStyles.colors.medium}
-            />}
-        </View>
+        <>
+            <TouchableWithoutFeedback onPress={() => setModalVisible(true)} >
+                <View style={styles.container} >
+                    {icon && <MaterialCommunityIcons
+                        name={icon}
+                        size={20}
+                        color={defaultStyles.colors.medium}
+                        style={styles.icon} />}
+                    <AppText style={styles.text} >{placeholder}</AppText>
+                    {icon && <MaterialCommunityIcons
+                        name='chevron-down'
+                        size={20}
+                        color={defaultStyles.colors.medium}
+                    />}
+                </View>
+            </TouchableWithoutFeedback>
+            <Modal visible={modalVisible} animationType='slide' >
+                <Screen>
+                    <Button title='Close' onPress={() => setModalVisible(false)} />
+                    <FlatList
+                        data={items}
+                        keyExtractor={item => item.value.toString()}
+                        renderItem={({ item }) =>
+                            <PickerItem
+                                label={item.label}
+                                onPress={() => setModalVisible(false)}
+                            />
+                        }
+
+                    />
+                </Screen>
+            </Modal>
+        </>
     )
 }
 
