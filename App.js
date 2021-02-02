c2fc0546d98b7c7faf7aabb7b559d9f85a89a0f2
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-02-02 17:26:20 -0500

    push from root

--- App.js
+++ App.js
@@ -34,14 +34,23 @@ import ListingsScreen from './app/screens/ListingsScreen';
 import AppTextInput from './app/components/AppTextInput';
 import AppPicker from './app/components/AppPicker';
 
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 },
+]
+
 export default function App() {
 
-  const [isNew, setIsNew] = useState(false);
+  const [category, setCategory] = useState(categories[0]);
 
   return (
 
     <Screen>
-      <AppPicker icon='apps' placeholder='Category' />
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={item => setCategory(item)}
+        items={categories} icon='apps' placeholder='Category' />
       <AppTextInput icon='email' placeholder='Email' />
     </Screen>
     // <ListingsScreen />
