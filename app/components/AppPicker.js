28f054728deb292531df3b5375fa6da508c5752f
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-02-02 15:36:19 -0500

    custom AppPicker styled with icon and chevreon.  Used AppTextInput code so pick is consistent with app brand

---
+++ app/components/AppPicker.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { TextInput, View, StyleSheet, Platform } from 'react-native'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+import defaultStyles from '../config/styles'
+import AppText from './AppText.js';
+
+function AppPicker({ icon, placeholder, ...otherProps }) {
+    return (
+        <View style={styles.container} >
+            {icon && <MaterialCommunityIcons
+                name={icon}
+                size={20}
+                color={defaultStyles.colors.medium}
+                style={styles.icon} />}
+            <AppText style={styles.text} >{placeholder}</AppText>
+            {icon && <MaterialCommunityIcons
+                name='chevron-down'
+                size={20}
+                color={defaultStyles.colors.medium}
+            />}
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: defaultStyles.colors.light,
+        borderRadius: 25,
+        flexDirection: 'row',
+        width: '100%',
+        padding: 15,
+        marginVertical: 10,
+    },
+    icon: {
+        marginRight: 10,
+    },
+    text: {
+        flex: 1,
+    }
+})
+
+export default AppPicker;
