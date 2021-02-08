97a1e733310a9d12e5511eafeb1db64ecc930181
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-02-08 10:45:34 -0500

    last push for forms branc -> picker form and creationg of loging  screen file

---
+++ app/screens/LoginScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import Screen from '../components/Screen';
+
+function LoginScreen(props) {
+    return (
+        <Screen>
+
+        </Screen>
+    );
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default LoginScreen;
