ce59ecb46196de70e930f6dd7b6d0abecefa2cf1
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-02-11 11:51:59 -0500

    created react native functional component snippet (rnfc).  Tested.

---
+++ app/components/Test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+function Test(props) {
+    return (
+        <View>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {},
+})
+
+export default Test;
