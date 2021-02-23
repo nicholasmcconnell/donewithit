ea3488f97734a6172e1b3fa446b69d3ef1224857
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-02-23 12:49:49 -0500

    added getLocation function to ListingEditScreen via expo-location and cal to async getLocation() via useEffect hook.

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import * as Yup from "yup";
+import * as Location from 'expo-location';
 
 import CategoryPickerItem from "../components/CategoryPickerItem";
 import FormImagePicker from '../components/forms/FormImagePicker';
@@ -77,6 +78,24 @@ const categories = [
     },
 ];
 function ListingEditScreen() {
+
+    const [location, setLocation] = useState();
+
+    const getLocation = async () => {
+        try {
+            const { granted } = await Location.requestPermissionsAsync();
+            if (!granted) return;
+            const { coords: { latitude, longitude } } = await Location.getLastKnownPositionAsync();
+            setLocation({ latitude, longitude });
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getLocation();
+    }, [])
+
     return (
         <Screen style={styles.container}>
             <AppForm
@@ -87,7 +106,7 @@ function ListingEditScreen() {
                     category: null,
                     images: [],
                 }}
-                onSubmit={(values) => console.log(values)}
+                onSubmit={(values) => console.log(location)}
                 validationSchema={validationSchema}
             >
                 <FormImagePicker name='images' />
