fd31665b23eba3b060149b754def520d669dfe36
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-02-23 15:05:26 -0500

    Implemented .navigate as prop to parent components.  And useNavigation hook to chiled compoents needing navigation capibilities

--- App.js
+++ App.js
@@ -1,35 +1,53 @@
 import * as React from 'react';
-import { Text } from 'react-native';
+import { Button, Text } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 
 import Screen from './app/components/Screen';
 
-const Tweets = () => {
+const Link = () => {
+  //this will be a child of tweets
+  //child cant use navigation prop or acces it in parrent.  Importing the hook gives child access to navigation
+
+  const navigation = useNavigation();
+
   return (
-    <Screen>
-      <Text>Tweets</Text>
-    </Screen>
+    <Button
+      title='Click'
+      onPress={() => navigation.navigate('TweetDetails')}
+    />
   )
 }
 
-const TweetDetails = () => {
-  return (
-    <Screen>
-      <Text>Tweet Details</Text>
-    </Screen>
-  )
-}
+
+const Tweets = ({ navigation }) => (
+  //navigation is speacial prop from @react-navigation
+  //Wher to navigte is specifgied in Button onPress
+  //navigation prop only availinble to screen components (Stack.Screen)
+  // child of screen component won't be sccesible via navigation prop
+  <Screen>
+    <Text>Tweets</Text>
+    <Link />
+    {/* <Button
+      title='View Tweet'
+      onPress={() => navigation.navigate('TweetDetails')}
+    /> */}
+  </Screen>
+);
+
+const TweetDetails = () => (
+  <Screen>
+    <Text>Tweet Details</Text>
+  </Screen>
+);
 
 const Stack = createStackNavigator();
-const StackNavigator = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name='Tweets' component={Tweets} />
-      <Stack.Screen name='TweetDetails' component={TweetDetails} />
-    </Stack.Navigator>
-  )
-}
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name='Tweets' component={Tweets} />
+    <Stack.Screen name='TweetDetails' component={TweetDetails} />
+  </Stack.Navigator>
+)
 
 
 export default function App() {
