9ca9cab4ee0f8fa0a0ff6b114cc65ad39658614c
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-03-15 14:31:48 -0400

    created get listsings fucntion to run in useEffect when loading listings screen

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,28 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FlatList, StyleSheet } from 'react-native';
 
 import Card from '../components/Card';
 import colors from '../config/colors';
+import listingsApi from '../api/listings';
 import routes from '../navigation/routes'
 import Screen from '../components/Screen';
 
-const listings = [
-    {
-        id: 1,
-        title: 'Red jacket for sale',
-        price: 100,
-        image: require('../assets/jacket.jpg'),
-    },
-    {
-        id: 2,
-        title: 'Couch in great condition',
-        price: 1000,
-        image: require('../assets/couch.jpg'),
-    },
-
-]
 //Because listingsScreen is registered with Navigator we have acces to navigation prop
 function ListingsScreen({ navigation }) {
+    const [listings, setListings] = useState([]);
+
+    useEffect(() => {
+        loadListings();
+    }, [])
+
+    const loadListings = async () => {
+        const res = await listingsApi.getListings()
+        setListings(res.data);
+    }
+
     return (
         <Screen style={styles.screen}>
             <FlatList
@@ -32,7 +29,7 @@ function ListingsScreen({ navigation }) {
                     <Card
                         title={item.title}
                         subTitle={'$' + item.price}
-                        image={item.image}
+                        imageUrl={item.images[0].url}
                         //pass second argument so listiing details screen knows what to render
                         onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
                     />
