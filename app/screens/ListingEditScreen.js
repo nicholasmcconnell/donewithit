574d319042d3ff344f5806657fc6aa74c17de2ea
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-03-23 22:41:34 -0400

    replaced text component showing progress with progress bar via react native progress

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -85,6 +85,7 @@ function ListingEditScreen() {
     const [progress, setProgress] = useState(0)
 
     const handleSubmit = async (listing) => {
+        setProgress(0);
         setUploadVisible(true);
         const res = await listingsApi.addListing({ ...listing, location }, progress => setProgress(progress))
         setUploadVisible(false);
--- app/screens/UploadScreen.js
+++ app/screens/UploadScreen.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { View, StyleSheet, Modal } from 'react-native';
 import AppText from '../components/AppText.js';
+import * as Progress from 'react-native-progress';
+import colors from '../config/colors.js';
+
 
 function UploadScreen({ progress = 0, visible = false }) {
     console.log(progress)
     return (
         <Modal visible={visible}>
             <View style={styles.container}>
-                <AppText>{progress * 100}%</AppText>
+                <Progress.Bar color={colors.primary} progress={progress} width={200} />
             </View>
         </Modal>
     );
