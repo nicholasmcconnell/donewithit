218836827a172935b0a94d55de81213c7fa1733c
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-03-23 21:07:59 -0400

    pussh before new branch creation

--- app/api/listings.js
+++ app/api/listings.js
@@ -4,6 +4,13 @@ const endpoint = '/listings';
 
 const getListings = () => client.get(endpoint);
 
+const addListing = listing => {
+    const data = new FormData();
+    data.append('title', listing.title)
+    data.append('price', listing.price)
+    data.append('categoryId', listing.category.value)
+}
+
 export default {
     getListings,
 }
