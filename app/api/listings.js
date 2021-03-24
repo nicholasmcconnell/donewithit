27d4b54495d89ae71b9e610c722b3dee3d5538a8
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-03-23 21:17:43 -0400

    created api call for posting new listings to the server

--- app/api/listings.js
+++ app/api/listings.js
@@ -9,8 +9,23 @@ const addListing = listing => {
     data.append('title', listing.title)
     data.append('price', listing.price)
     data.append('categoryId', listing.category.value)
+    data.append('description', lising.description)
+
+    listing.images.forEach((image, index) =>
+        data.append('images', {
+            name: 'image' + index,
+            type: 'image/jpeg',
+            uri: image,
+        }))
+    if (listing.location) {
+        data.append('location', JSON.stringify(listing.location));
+    }
+
+    return client.post(endpoint, data);
+
 }
 
 export default {
+    addListing,
     getListings,
 }
